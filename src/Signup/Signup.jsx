b0c43ff286b4aa768a4a32d77f0a35701bdf8d1e
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-06-05 20:45:39 +0600

    signup page connected to backend

--- src/Signup/Signup.jsx
+++ src/Signup/Signup.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Signup.css'
 const Signup = () => {
+    const [fullname,setFullname]=useState('');
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    async function handleRegister(e){
+        e.preventDefault()
+        await fetch('http://localhost:4000/register',{
+            method:'POST',
+            body:JSON.stringify({fullname,email,password}),
+            headers:{'Content-Type':'application/json'}
+           
+
+
+        })
+    }
+
+
+
     return (
         <>
              <section>
@@ -10,10 +28,27 @@ const Signup = () => {
                         <h1 className='text-center text-3xl my-3'><Link to='/'>Code blog</Link></h1>
 
                         <div className='  '>
-                            <form action="">
-                                <input type="text" className=' w-full h-11 border-b-2 border-gray-400 p-2 my-3' placeholder='your name'  />
-                                <input type="text" className=' w-full h-11 border-b-2 p-2 border-gray-400 my-3' placeholder='yourmail@email.com'  />
-                                <input type="password" className=' w-full h-11 border-b-2 border-gray-400 p-2 my-3' placeholder='password'  />
+                            <form action="" onSubmit={handleRegister}>
+                                <input type="text" className=' w-full h-11 border-b-2 border-gray-400 p-2 my-3'
+                                    placeholder='your name' 
+                                    value={fullname}
+                                    onChange={(e)=>setFullname(e.target.value) }
+                                    
+                                />
+                                <input type="text" className=' w-full h-11 border-b-2 p-2 border-gray-400 my-3'
+                                    placeholder='yourmail@email.com'
+                                    value={email}
+                                    onChange={(e)=> setEmail(e.target.value)}
+
+                                 
+                                 />
+                                <input type="password" className=' w-full h-11 border-b-2 border-gray-400 p-2 my-3' 
+                                    placeholder='password'
+                                    value={password}
+                                    onChange={(e)=> setPassword(e.target.value)}
+
+                                
+                                />
                                 <button className='signup-btn'>Sign up</button>
                                 <p className='py-3 text-center text-gray-600 hover:text-indigo-500'><Link to='/login'>Already have an account? Log in</Link></p>
                             </form>
